6189e3148177553c7a986182c87decc1f65e2da4
joseph-dany <yalanatiges@gmail.com>
2024-12-12 10:29:32 +0530

    Update Parcel.py

--- jd1/Parcel.py
+++ jd1/Parcel.py
@@ -8,6 +8,27 @@ from forms.contact import contact_form
 @st.dialog("Contact me")
 def show_contact_form():
     contact_form()
+def contact_form():
+    with st.form("contact_form"):
+        name=st.text_input("First Name")
+        number=st.text_input("Contact number")
+        present=st.text_input("From")
+        where=st.text_input("To where")
+        msg=st.text_area("Your message")
+        submit_but=st.form_submit_button("submit")
+
+        if submit_but:
+            st.success("Message successfully sent!")
+st.title("Timing :mantelpiece_clock:")
+
+st.subheader("Know the Timings of buses you want :dart:")
+st.write("""
+* Kurnool to Hyderabad :point_right:[k2h.list>](https://docs.google.com/spreadsheets/d/1Cts-Y6vJB_e55_YYZG_QUp3SzhmVQmehStqKdNWaxdk/edit?usp=sharing)
+* Kurnool  to Bangalore:point_right:[k2b.list>](https://docs.google.com/spreadsheets/d/1vG5q_LBV-LhKcNnlHG9v6s9aHVuz14se6YGpf9G2alk/edit?usp=sharing)
+* Kurnool to Tirupati :point_right:[k2t.list>](https://docs.google.com/spreadsheets/d/1Z0u5lpXS9tLtKOVISxjzjxqBFFwLbssayE7zh-sJSpQ/edit?usp=sharing)
+* Kurnool to Vijayawada:point_right:[ktv.list>](https://docs.google.com/spreadsheets/d/1ictO8SNqlm27GCe8p0p7bS19fPHV-vCYNiVw6v6rcmM/edit?usp=sharing)""")
+st.write("---")
+st.write("May the timings can be delay but you can catch the bus with in or after 15-20 mins . We are assure you that be careful with online payment don't give or send money to unknown :thumbsup:")
 
 st.title("Businfoo! :trolleybus:")
 col1,col2=st.columns(2,gap="small")
